a18bc9266bdd27cd30fc2c16b31c598c5bc6c365
Kara Bayse <karabayse@gmail.com>
2021-02-19 17:58:11 -0600

    min window substring

--- test.js
+++ test.js
@@ -12,10 +12,28 @@ located at the end of the string.
 function minWindowSubstring(strArr) {
 // strArr contains 2 strings: N and K
 // K is smaller than N
-
-// check for versions of K within N
-  let len = k.length;
-  let substrng = K(0,len);
+  const subIncludesAll = (str, str2) => {
+    for (let i = 0; i < str.length; i++) {
+        if (str2.indexOf(str[i]) !== -1) {
+          str2 = str2.replace(str[i], '');
+        };
+      };
+      return (str2.length === 0);
+    };
+  const minWindow = (str1 = '', str2 = '') => {
+    let shortestString = null;
+    for (let i = 0; i < str1.length; i++) {
+      for (let j = i; j < str1.length; j++) {
+        let testString = str1.substr(i, j-i+1);
+        if (subIncludesAll(testString, str2)) {
+            if (shortestString === null || testString.length < shortestString.length) {
+              shortestString = testString;
+            }
+          }
+        }
+      }
+    return shortestString;
+  };
 }
 
-minWindowSubstring();
+minWindowSubstring(["aaabaaddae", "aed"]);
