5b7100dcde9ac9bb282b163211c5333294eefb2c
Kara Bayse <karabayse@gmail.com>
2021-02-20 18:21:35 -0600

    min window substring

--- coderbyte.js
+++ coderbyte.js
@@ -10,7 +10,30 @@ then the smallest substring of N that contains the characters a, e, and d is "da
 located at the end of the string.
 */
 function minWindowSubstring(strArr) {
-
+  // strArr contains 2 strings: N and K
+  // K is smaller than N
+    const subIncludesAll = (str, str2) => {
+      for (let i = 0; i < str.length; i++) {
+          if (str2.indexOf(str[i]) !== -1) {
+            str2 = str2.replace(str[i], '');
+          };
+        };
+        return (str2.length === 0);
+      };
+    const minWindow = (str1 = '', str2 = '') => {
+      let shortestString = null;
+      for (let i = 0; i < str1.length; i++) {
+        for (let j = i; j < str1.length; j++) {
+          let testString = str1.substr(i, j-i+1);
+          if (subIncludesAll(testString, str2)) {
+              if (shortestString === null || testString.length < shortestString.length) {
+                shortestString = testString;
+              }
+            }
+          }
+        }
+      return shortestString;
+    };  
 }
 
 minWindowSubstring();
