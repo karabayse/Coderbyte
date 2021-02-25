edab987be2a176f84125fe7e7dfeeafe3fd9b2ea
Kara Bayse <karabayse@gmail.com>
2021-02-24 19:57:16 -0600

    first factorial

--- test.js
+++ test.js
@@ -5,7 +5,13 @@ if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
 For the test cases, the range will be between 1 and 18 and the input will always be an integer.
 */
 function FirstFactorial(num) {
-
+  if (num < 0)
+        return -1;
+  else if (num == 0)
+      return 1;
+  else {
+      return (num * FirstFactorial(num - 1));
+  }
 }
 
-FirstFactorial();
+FirstFactorial(5);
