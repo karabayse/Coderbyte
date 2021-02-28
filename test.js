cf4984e99eb548768fdbd7ce89f210f311689087
Kara Bayse <karabayse@gmail.com>
2021-02-27 18:37:15 -0600

    longest word

--- test.js
+++ test.js
@@ -1,23 +1,11 @@
-/*
-Take the sen parameter being passed and return the largest word in the string.
-If there are two or more words that are the same length, return the first word
-from the string with that length.
-Ignore punctuation and assume sen will not be empty.
-Examples
-Input: "fun&!! time"
-Output: time
-*/
 function LongestWord(sen) {
   var strSplit = sen.split(' ');
-  console.log(strSplit);
+  console.log(strSplit); // ["Longest", "is", "the", "longest", "word", "in", "this", "string"]
 
   var longestWord = 0;
   for(var i = 0; i < strSplit.length; i++){
-    if(strSplit[i].length > longestWord){
-	     longestWord = strSplit[i].length;
+
      }
   }
-  console.log(longestWord);
-}
 
 LongestWord("Longest is the longest word in this string");
