9c562cdbb92ab217c1556ef90e14108696570a5e
Kara Bayse <karabayse@gmail.com>
2021-03-04 19:09:42 -0600

    longest word

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ function LongestWord(sen) {
   var strSplit = sen.split(' ');
   var longest = strSplit.reduce(
     function (a, b) {
-      console.log(a.length > b.length ? a : b);
+      return a.length > b.length ? a : b;
     }
   );
+  console.log(longest);
 }
 
 LongestWord("Longest is the longest word in this string");
