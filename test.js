d90be40019d40f9665cd454911cd08a92093842b
Kara Bayse <karabayse@gmail.com>
2021-03-09 18:51:07 -0600

    question marks

--- test.js
+++ test.js
@@ -7,9 +7,15 @@ If so, then your program should return the string true,
 otherwise it should return the string false.
 If there aren't any two numbers that add up to 10 in the string,
 then your program should return false as well.
+
+For example: if str is "arrb6???4xxbl5???eee5"
+then your program should return true because there are exactly 3 question marks
+between 6 and 4, and 3 question marks between 5 and 5 at the end of the string.
 */
 function QuestionsMarks(str) {
-
+  if (str.includes("???")) {
+    var oneNum = addy.substr(0, addy.indexOf(',')); 
+  }
 }
 
 QuestionMarks();
