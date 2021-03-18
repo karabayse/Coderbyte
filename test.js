ee376ef90efddcd56417c6669dc2fd90a30cd066
Kara Bayse <karabayse@gmail.com>
2021-03-17 19:58:57 -0500

    find intersection

--- test.js
+++ test.js
@@ -4,14 +4,17 @@ strArr will contain 2 elements:
 ascending order.
 --The second element will represent a second list of comma-separated numbers (also sorted).
 
-Return a comma-separated string containing the numbers that occur in elements of
-strArr in sorted order.
+Return a comma-separated string containing the numbers that occur in both
+elements of strArr in sorted order.
 
 If there is no intersection, return the string false.
 
-Example
+Examples
 Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
 Output: 1,4,13
+
+Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+Output: 1,9,10
 */
 function FindIntersection(strArr) {
 
