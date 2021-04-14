a0e5a5dee85bfe662833f0ca4e63546f95a6344c
Kara Bayse <karabayse@gmail.com>
2021-04-14 18:59:04 -0500

    bracket matcher

--- coderbyte.js
+++ coderbyte.js
@@ -175,3 +175,27 @@ function FindIntersection(strArr) {
 }
 
 FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]);
+
+
+/*
+The str parameter being passed should return 1 if the brackets are correctly
+matched and each one is accounted for.
+Otherwise return 0.
+
+For example:
+if str is "(hello (world))", then the output should be 1,
+but if str is "((hello (world))" the the output should be 0
+because the brackets do not correctly match up.
+Only "(" and ")" will be used as brackets.
+If str contains no brackets, return 1.
+*/
+function BracketMatcher(str) {
+  var arr = str.split(" ");
+  console.log(arr);
+
+  for (var i = 0; i < arr.length; i++) {
+    // check for ( and )
+  }
+}
+
+BracketMatcher();
--- test.js
+++ test.js
@@ -8,10 +8,12 @@ if str is "(hello (world))", then the output should be 1,
 but if str is "((hello (world))" the the output should be 0
 because the brackets do not correctly match up.
 Only "(" and ")" will be used as brackets.
-If str contains no brackets return 1.
+If str contains no brackets, return 1.
 */
 function BracketMatcher(str) {
   var arr = str.split(" ");
+  console.log(arr);
+  
   for (var i = 0; i < arr.length; i++) {
     // check for ( and )
   }
